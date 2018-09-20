7d509c1ec5a65a8c306c533ff6d95355592dc3c3
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-09-20 15:59:57 +0300

    update on release info

---
+++ ncryptix/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This package encrypts and decrypts data from remote server.
+"""
+
+
+# Source code meta data
+__author__ = 'Dalwar Hossain'
+__email__ = 'dalwar.hossain@protonmail.com'
