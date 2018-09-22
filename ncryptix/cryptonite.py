f386ee4d081255490328ef227128646e1891ab1a
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-09-22 13:32:10 +0300

    adding cryptonite

---
+++ ncryptix/cryptonite.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+
+# Source code meta data
+__author__ = 'Dalwar Hossain'
+__email__ = 'dalwar.hossain@protonmail.com'
+
+
+# Check internet connection
+def _connected_to_internet():
+    pass
+
+
+# Check if the remote host is alive
+def _is_alive(remote_host=None):
+    pass
+
+
+# Check if the remote host has data or not
+def _has_data(remote_host=None, directory=None):
+    pass
